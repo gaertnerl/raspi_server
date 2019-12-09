f0a4dfe83ba99ec03ca459cc22577f1d91ebce02
Laergner <gaertnerleonard@web.de>
2019-12-09 20:57:10 +0100

    development routes added

---
+++ app/blueprints/development.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, jsonify
+from werkzeug.security import generate_password_hash
+
+from ..utilities.auth import password_requirements, username_requirements
+from ..database import db, User, check_allowed_characters
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/create_super_user', methods=['POST'])
+def create_super_user():
+    """ Create an admin user for development purposes.
+    :return: ok message
+    """
+
+    super_u = User(
+        username='super',
+        password=generate_password_hash('super'),
+        admin=True,)
+
+    db.session.add(super_u)
+    db.session.commit()
+
+    return jsonify({'message': 'super user created'}), 200
+
+
+@bp.route('/ping', methods=['GET'])
+def pong():
+    return jsonify({'message': 'pong'}), 200
